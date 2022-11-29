f8315874eb1ab6d1ae402726e3b090741a677cd1
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2022-11-29 19:32:51 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import "./App.css"
+
+
+function App() {
+
+  return (
+    <div className="">
+
+      <h1 className="underline text-2xl">commentss</h1>
+
+    </div>
+  )
+}
+
+export default App
