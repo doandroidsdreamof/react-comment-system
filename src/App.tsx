09dd415ab2d70ac270ae3d69499577a945be9fd2
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2022-11-29 22:10:43 +0300

    vite .env config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Comment from './components/comments/Comment'
+import {auth} from './firebase'
 
 function App() {
 const deneme: string = 'deneme'
+
+console.log("🚀 ~ file: App.tsx:10 ~ App ~ auth", auth)
+
 
   return (
     <div className=''>
@@ -13,5 +17,7 @@ const deneme: string = 'deneme'
     </div>
   )
 }
+
+
 
 export default App
