b308ee538d4ebd19b43fe1c3990ec7dad29beee7
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2022-11-29 20:26:48 +0300

    elint && prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import "./App.css"
-
+import './App.css'
+import Comment from './components/Comment'
 
 function App() {
-
   return (
-    <div className="">
-
-      <h1 className="underline text-2xl">commentss</h1>
-
+    <div className=''>
+      <h1 className='underline text-2xl '>comments</h1>
     </div>
   )
 }
