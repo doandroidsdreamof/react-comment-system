227184ba873aecb73af448ebef56358bc3d7848e
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2022-12-01 02:09:41 +0300

    form style && responsive settings

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,37 +1,42 @@
-import React from 'react'
+import React, {useState,useEffect,useContext} from 'react'
 
 import LoginResetPassword from './LoginResetPassword'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { Field, Form, Formik } from 'formik'
+
+
+// alert popups //
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from 'react-toastify'
+import { injectStyle } from 'react-toastify/dist/inject-style'
 
 const LoginForm = () => {
   return (
     <>
       <div className='flex flex-col items-end'>
-        <div className='w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400  focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300'>
-          <input
-            id=''
-            type='Your password'
-            placeholder='Email address'
-            className='w-full bg-transparent pb-3  border-b border-gray-300 outline-none  invalid:border-red-400 transition'
-          />
-        </div>
+        <input
+          type='email'
+          name='email'
+          id='email'
+          placeholder='email'
+          className='focus:outline-none block w-full rounded-md border border-gray-400  bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300'
+        />
       </div>
       <div className='flex flex-col items-end'>
-        <div className='w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400  focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300'>
-          <input
-            id=''
-            type='Your password'
-            placeholder='Password'
-            className='w-full bg-transparent pb-3  border-b border-gray-300  outline-none  invalid:border-red-400 transition'
-          />
-        </div>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          placeholder='password'
+          className='focus:outline-none block w-full rounded-md border border-gray-400  bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300'
+        />
         <LoginResetPassword />
         <div className='w-full'>
-        <button className='w-full rounded-full bg-sky-500 h-11 flex items-center justify-center px-6 py-3 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-800'>
-          <span className='text-base font-semibold text-white '>Login</span>
-        </button>
+          <button className='w-full rounded-full bg-sky-500 h-11 flex items-center justify-center px-6 py-3 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-800'>
+            <span className='text-base font-semibold text-white '>Login</span>
+          </button>
+        </div>
       </div>
-      </div>
-
     </>
   )
 }
--- src/components/register/RegisterForm.jsx
+++ src/components/register/RegisterForm.jsx
@@ -1,50 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// alert popups //
+import 'react-toastify/dist/ReactToastify.css'
 
 const RegisterForm = () => {
+  const [name, setName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const formValidation = (e) => {
+    e.preventDefault()
+    const emailValidation = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+    alert('ok')
+  }
+
   return (
     <>
       <div className='flex flex-col items-end'>
-
-        <div className='w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400  focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300'>
-          <input
-            id=''
-            type=''
-            placeholder='Name'
-            className='w-full bg-transparent pb-3  border-b border-gray-300  outline-none  invalid:border-red-400 transition'
-          />
-        </div>
+        <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder='name'
+                className="focus:outline-none block w-full rounded-md border border-gray-400  bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+             />
       </div>
       <div className='flex flex-col items-end'>
-        <div className='w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400  focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300'>
-          <input
-            id=''
-            type=''
-            placeholder='Last Name'
-            className='w-full bg-transparent pb-3  border-b border-gray-300  outline-none  invalid:border-red-400 transition'
-          />
-        </div>
+        <input
+                type="text"
+                name="last name"
+                id="last name"
+                placeholder='last name'
+                className="focus:outline-none block w-full rounded-md border border-gray-400  bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+             />
+      </div>
+      <div className='flex flex-col '>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder='email'
+                className="focus:outline-none block w-full rounded-md border border-gray-400  bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+             />
       </div>
       <div className='flex flex-col items-end'>
-        <div className='w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400  focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300'>
-          <input
-            id=''
-            type=''
-            placeholder='Email'
-            className='w-full bg-transparent pb-3  border-b border-gray-300  outline-none  invalid:border-red-400 transition'
-          />
-        </div>
+        <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder='password'
+                className="focus:outline-none block w-full rounded-md border border-gray-400  bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+             />
       </div>
-      <div className='flex flex-col items-end'>
-        <div className='w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400  focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300'>
-          <input
-            id=''
-            type=''
-            placeholder='Password'
-            className='w-full bg-transparent pb-3  border-b border-gray-300  outline-none  invalid:border-red-400 transition'
-          />
-        </div>
-      </div>
-      <button className='w-full rounded-full bg-sky-500  h-11 flex items-center justify-center px-6 py-3 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-800'>
+      <button
+        onSubmit={formValidation}
+        className='w-full rounded-full bg-sky-500  h-11 flex items-center justify-center px-6 py-3 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-800'
+      >
         <span className='text-base font-semibold text-white '>Register</span>
       </button>
     </>
