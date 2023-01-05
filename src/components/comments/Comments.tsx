14545855520cb356a6728b936d799be9818246a4
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-01-05 19:35:43 +0300

    sort reply-comments by createdAt

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -5,7 +5,7 @@ import { AuthContext } from '../../context/AuthContext';
 import {
   getAuth,
 } from 'firebase/auth';
-import { collection, getDocs, query, where, deleteDoc, doc, updateDoc } from 'firebase/firestore'
+import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../../firebase';
 // local imports //
 import ReplyCommentForm from './ReplyCommentForm';
@@ -14,9 +14,9 @@ import removeComment from '../../hooks/removeComment';
 import EditForm from './EditForm';
 // redux //
 import { useDispatch, useSelector } from 'react-redux'
-import { commentObserver, removedObserver } from '../../store/reducers/userSlice'
+import { removedObserver } from '../../store/reducers/userSlice'
 // interfaces //
-import { CommentsData, ReplyCommentsData } from '../../types/interfaces'
+import { CommentsData } from '../../types/interfaces'
 // image //
 import fallBack from '../../assets/images/fallback-image.jpg'
 
@@ -66,8 +66,12 @@ const Comments: React.FC<CommentsData> = ({ items }: CommentsData[]) => {
       const getData: any[] = []
       querySnapshot.forEach((doc) => {
         getData.push(doc.data())
+
       })
+
+      getData.sort(function (a, b) { return b.createdAt - a.createdAt })
       setReply([...getData])
+
     }
     catch (err) {
       console.error(err)
@@ -78,7 +82,7 @@ const Comments: React.FC<CommentsData> = ({ items }: CommentsData[]) => {
   return (
     <>
       <EditForm id={items.postID} text={items.text} close={editModal} toggle={(e) => setEditModal(false)} />
-      <article className={editModal ? "hidden" : "p-6 mb-6    text-base bg-white rounded-lg  shadow-sm"}>
+      <article className={editModal ? 'hidden' : 'p-6 mb-6    text-base bg-white rounded-lg  shadow-sm'}>
         <footer className="flex justify-between items-center mb-2">
           <div className="flex items-center">
             <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><img
@@ -90,11 +94,11 @@ const Comments: React.FC<CommentsData> = ({ items }: CommentsData[]) => {
           </div>
           <div className="  ml-auto  ">
             <button
-              className={items.userID === user?.uid ? " inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 " : "hidden"}
+              className={items.userID === user?.uid ? ' inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 ' : 'hidden'}
               type="button"
               onClick={() => setModal(!modal)}
             >
-              <svg className={items?.userName === "removed" ? "hidden" : "w-5 h-5"} aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+              <svg className={items?.userName === 'removed' ? 'hidden' : 'w-5 h-5'} aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z">
@@ -104,7 +108,7 @@ const Comments: React.FC<CommentsData> = ({ items }: CommentsData[]) => {
             </button>
             <div
               className={modal ? 'absolute z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow ' : 'hidden'}>
-              <ul className={items?.userName === "removed" ? "hidden" : "py-1 text-sm text-gray-700"}
+              <ul className={items?.userName === 'removed' ? 'hidden' : 'py-1 text-sm text-gray-700'}
                 aria-labelledby="dropdownMenuIconHorizontalButton">
                 <li>
                   <button
@@ -124,13 +128,13 @@ const Comments: React.FC<CommentsData> = ({ items }: CommentsData[]) => {
         <p className="text-gray-500 ">
           {items?.text}
         </p>
-        <div className={items?.text && items?.userName === 'removed' ? 'hidden' : "flex items-center mt-4 space-x-4"}>
+        <div className={items?.text && items?.userName === 'removed' ? 'hidden' : 'flex items-center mt-4 space-x-4'}>
           <button
             id={items?.postID}
             onClick={(e) => { replyComment(e) }}
             type="button"
             className="flex items-center text-sm text-gray-500 hover:underline ">
-            <svg aria-hidden="true" className={"mr-1 w-4 h-4"} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
+            <svg aria-hidden="true" className={'mr-1 w-4 h-4'} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
             Reply
           </button>
         </div>
@@ -138,7 +142,7 @@ const Comments: React.FC<CommentsData> = ({ items }: CommentsData[]) => {
       <ReplyCommentForm ID={items.postID} close={open} open={(e) => setOpen(false)} />
       {
         reply.map((data, index) => (
-          <ReplyComments replyComments={data} key={index} />
+          <ReplyComments replyComments={data} key={data.postID} />
         ))
       }
 
